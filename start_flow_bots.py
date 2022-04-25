260b78cd16604c1524f71bb6e71b317c6bf20c4f
Roman <graff049@gmail.com>
2022-04-25 21:55:39 +0300

    1. test detect_intent_texts

--- start_flow_bots.py
+++ start_flow_bots.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 
+from dialog_flow_tools import detect_intent_texts
 from telegram_bot import TelegramFlowBot
 
 
@@ -17,8 +18,13 @@ def main():
     load_dotenv()
     telegram_token = os.getenv('TG_TOKEN')
     google_app_creds = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+    project_id = 'useful-assistant-bot-mgib'
+    session_id = '123456789'
+    texts = ['пРИвет!']
+    language_code = 'ru'
 
-    start_tg_dialog_bot(telegram_token)
+    detect_intent_texts(project_id=project_id, session_id=session_id, texts=texts, language_code=language_code)
+    # start_tg_dialog_bot(telegram_token)
 
 
 if __name__ == '__main__':
