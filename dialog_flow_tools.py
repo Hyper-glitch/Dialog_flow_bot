816f84626b1f87214203efb1044734175352be99
Roman <graff049@gmail.com>
2022-05-19 17:55:12 +0300

    1. renaming

--- dialog_flow_tools.py
+++ dialog_flow_tools.py
@@ -10,7 +10,6 @@ def detect_intent_texts(text_from_user):
     session = session_client.session_path(project='useful-assistant-bot-mgib', session='123456789')
     text_input = dialogflow.TextInput(text=text_from_user, language_code='ru')
     query_input = dialogflow.QueryInput(text=text_input)
-
-    response = session_client.detect_intent(request={"session": session, "query_input": query_input})
-    detected_answer = response.query_result.fulfillment_text
-    return detected_answer
+    response = session_client.detect_intent(request={'session': session, 'query_input': query_input})
+    dialogflow_answer = response.query_result.fulfillment_text
+    return dialogflow_answer
