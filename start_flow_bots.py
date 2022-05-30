07a9698d027d970db49351364b97ceb2746582d4
Roman <graff049@gmail.com>
2022-05-30 23:18:28 +0300

    1. sort import 2. pep8 space

--- start_flow_bots.py
+++ start_flow_bots.py
@@ -1,5 +1,7 @@
 import os
 
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
 from dotenv import load_dotenv
 
 from telegram_bot import TelegramFlowBot
@@ -12,11 +14,27 @@ def start_tg_bot(telegram_token):
     tg_flow_instance.start_bot()
 
 
+def start_vk_bot(vk_token):
+    vk_session = vk_api.VkApi(token=vk_token)
+    longpoll = vk_api.longpoll.VkLongPoll(vk_session)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            print('Новое сообщение:')
+            if event.to_me:
+                print('Для меня от: ', event.user_id)
+            else:
+                print('От меня для: ', event.user_id)
+            print('Текст:', event.text)
+
+
 def main():
     """The main logic for starting dialog flow bot."""
     load_dotenv()
     telegram_token = os.getenv('TG_TOKEN')
-    start_tg_bot(telegram_token)
+    vk_token = os.getenv('VK_GROUP_TOKEN')
+    # start_tg_bot(telegram_token)
+    start_vk_bot(vk_token)
 
 
 if __name__ == '__main__':
