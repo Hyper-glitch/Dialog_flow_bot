5af777785bc469e6c2557fb763de51a3264babf2
Roman <graff049@gmail.com>
2022-05-30 23:24:23 +0300

    1. add echo bot to vk 2. sort import

--- start_flow_bots.py
+++ start_flow_bots.py
@@ -1,8 +1,9 @@
 import os
+import random
 
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+import vk_api as vk
 from dotenv import load_dotenv
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 from telegram_bot import TelegramFlowBot
 
@@ -15,17 +16,17 @@ def start_tg_bot(telegram_token):
 
 
 def start_vk_bot(vk_token):
-    vk_session = vk_api.VkApi(token=vk_token)
-    longpoll = vk_api.longpoll.VkLongPoll(vk_session)
+    vk_session = vk.VkApi(token=vk_token)
+    longpoll = vk.longpoll.VkLongPoll(vk_session)
+    vk_api = vk_session.get_api()
 
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            vk_api.messages.send(
+                user_id=event.user_id,
+                message=event.text,
+                random_id=random.randint(1, 1000),
+            )
 
 
 def main():
